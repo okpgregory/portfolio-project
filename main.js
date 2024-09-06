aa0d914e81a5532831c8b7e14ffff1fc1ec5903d
Okpeahior Gregory <greghim53lf+github@gmail.com>
2024-09-06 07:13:07 +0100

    second commit

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
+// THEME SWITCHER
+
+const prefersDarkScheme = window.matchMedia(
+  "(prefers-color-scheme: dark)"
+).matches;
+
 let darkMode = localStorage.getItem("dark-mode");
-const themeButton = document.getElementById("theme-button");
+
+if (darkMode === null) {
+  if (prefersDarkScheme) {
+    localStorage.setItem("dark-mode", "enabled");
+  }
+}
+
+const themeButtons = document.getElementsByClassName("theme-button");
 
 const enableDarkMode = () => {
   document.body.classList.add("dark");
@@ -8,18 +21,47 @@ const enableDarkMode = () => {
 
 const disableDarkMode = () => {
   document.body.classList.remove("dark");
-  localStorage.setItem("dark-mode", null);
+  localStorage.setItem("dark-mode", "disabled");
 };
 
 if (darkMode === "enabled") {
   enableDarkMode();
 }
 
-themeButton.addEventListener("click", () => {
-  darkMode = localStorage.getItem("dark-mode");
-  if (darkMode === "enabled") {
-    disableDarkMode();
-  } else {
-    enableDarkMode();
-  }
-});
+for (let i = 0; i < themeButtons.length; i++) {
+  themeButtons[i].addEventListener("click", () => {
+    darkMode = localStorage.getItem("dark-mode");
+    if (darkMode === "enabled") {
+      disableDarkMode();
+    } else {
+      enableDarkMode();
+    }
+  });
+}
+
+// MOBILE MENU
+
+const isMenuOpen = false;
+
+const mobileMenu = document.querySelector(".mobile-menu");
+const overlay = document.querySelector(".overlay");
+const mobileMenuOpenButton = document.querySelector(".hamburger-menu");
+const mobileMenuCloseButton = document.querySelector(".close-button");
+
+const openMenu = () => {
+  mobileMenu.style.display = "block";
+  overlay.style.display = "block";
+  document.body.style.overflow = "hidden";
+};
+
+const closeMenu = () => {
+  mobileMenu.style.display = "none";
+  overlay.style.display = "none";
+  document.body.style.overflow = "auto";
+};
+
+closeMenu();
+
+mobileMenuOpenButton.addEventListener("click", openMenu);
+mobileMenuCloseButton.addEventListener("click", closeMenu);
+overlay.addEventListener("click", closeMenu);
